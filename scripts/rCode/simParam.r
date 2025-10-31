abf60fd7f0960166c37eee6de8ff50f43b08112c
adrose <adon.rosen@gmail.com>
2025-10-31 16:07:08 -0500

    reduce sim params

--- scripts/rCode/simParam.r
+++ scripts/rCode/simParam.r
@@ -1,14 +1,14 @@
 ## May 29th 2025
-sim.param.nitems <- c(7,14,21,28)
-sim.param.ncates <- c(3,5)
-sim.param.discri <- c(.6,2)
+sim.param.nitems <- c(10,20)
+sim.param.ncates <- c(3,6)
+sim.param.discri <- c(1,2.3)
 sim.param.2pl.spread <- c(3)
 sim.param.sample <- c(15000)
 sim.param.faccor <- c(.4,.8)
 sim.param.difgrmF <- c(-3,-1)
 sim.param.difgrmC <- c(1)
-sim.param.dif2pl <- c(-2,1)
-sim.param.discri2 <- c(.6,2)
+sim.param.dif2pl <- c(-2,0)
+sim.param.discri2 <- c(1,2.3)
 sim.iter <- 1:50
 all.sim.vals <- expand.grid(sim.param.nitems, sim.param.ncates, sim.param.discri, 
                             sim.param.2pl.spread,sim.param.sample, sim.param.faccor, 
